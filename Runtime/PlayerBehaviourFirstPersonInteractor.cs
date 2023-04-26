a85c4fca37d84b279d1d8a4d7b3806bb56571bdd
Iseeicy <iseeicyhawkston@gmail.com>
2023-04-25 21:38:46 -0500

    Fixed namespaces

--- Runtime/PlayerBehaviourFirstPersonInteractor.cs
+++ Runtime/PlayerBehaviourFirstPersonInteractor.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace BobboNet.Player
+namespace BobboNet.PlayerBehaviours
 {
     public class PlayerBehaviourFirstPersonInteractor : PlayerBehaviour
     {
